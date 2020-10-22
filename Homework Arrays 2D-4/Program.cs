a5287e329c1cb559065f47d330136db0cdecdbc6
Kirill Kireev <kereeevkirill@gmail.com>
2020-10-22 18:45:57 +0300

    Решенные, но не красивые

---
+++ Homework Arrays 2D-4/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Homework_Arrays_2D_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Найти индекс максимального элемента массива");
+            Console.WriteLine("Укажите количество переменных:");
+            Console.Write("N=");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("N1=");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+            int[,] a = new int[n, n1];
+            Random r = new Random();
+            for (int i = 0; i < a.GetLength(0); i++)
+            {
+                for (int j = 0; j < a.GetLength(1); j++)
+                {
+                    a[i, j] = r.Next(-100, 100);
+
+                }
+            }
+            Console.WriteLine("Исходный массив: ");
+            for (int i = 0; i < a.GetLength(0); i++)
+            {
+                for (int j = 0; j < a.GetLength(1); j++)
+                {
+
+                    //if (a[i, j] / 10 != 0)
+                    {
+                        Console.Write("  {0} ", a[i, j]);
+                    }
+                    //else 
+                    //{
+                    //    Console.Write("{ 0 } ", a[i, j]);
+                    //}
+                }
+                Console.WriteLine();
+            }
+            int max = a[0, 0];
+            int indexI = 0;
+            int indexJ = 0;
+            for (int i = 0; i < a.GetLength(0); i++)
+            {
+                for (int j = 0; j < a.GetLength(1); j++)
+
+                    if (max <= a[i, j])
+                    {
+                        max = a[i, j];
+                        indexI = i;
+                        indexJ = j;
+                    }
+            }
+            Console.WriteLine();
+            Console.WriteLine("Наибольший элемент массива: " + max);
+            Console.WriteLine("Индекс наибольшего элемента массива: (" + indexI + ", " + indexJ + ")");
+        }
+    }
+}
+
+
+
