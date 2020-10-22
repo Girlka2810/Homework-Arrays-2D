050336568c53b409a53ffeb3e0b5332f4791ac13
Kirill Kireev <kereeevkirill@gmail.com>
2020-10-22 22:42:07 +0300

    Сделанные, красивые)

--- Homework Arrays 2D - 5/Program.cs
+++ Homework Arrays 2D - 5/Program.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 
-namespace Homework_Arrays_2D_4
+namespace Homework_Arrays_2D___5
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Найти индекс максимального элемента массива");
+            Console.WriteLine("Найти количество элементов массива, " +
+                "которые больше своих левого, правого, верхнего и нижнего соседа одновременно.");
             Console.WriteLine("Укажите количество переменных:");
             Console.Write("N=");
             int n = Convert.ToInt32(Console.ReadLine());
@@ -14,12 +15,12 @@ namespace Homework_Arrays_2D_4
             int n1 = Convert.ToInt32(Console.ReadLine());
             int[,] a = new int[n, n1];
             Random r = new Random();
+            int count = 0;
             for (int i = 0; i < a.GetLength(0); i++)
             {
                 for (int j = 0; j < a.GetLength(0); j++)
                 {
-                    a[i, j] = r.Next(-100, 100);
-
+                    a[i, j] = r.Next(0, 10);
                 }
             }
             Console.WriteLine("Исходный массив: ");
@@ -27,31 +28,32 @@ namespace Homework_Arrays_2D_4
             {
                 for (int j = 0; j < a.GetLength(1); j++)
                 {
-                    {
-                        Console.Write(string.Format("{0,3} ", a[i, j]));
-                    }
+                                     
+                        Console.Write(string.Format("{0,4} ", a[i, j]));
                 }
-                Console.WriteLine();
+            Console.WriteLine();
             }
-            int max = a[0, 0];
-            int indexI = 0;
-            int indexJ = 0;
             for (int i = 0; i < a.GetLength(0); i++)
             {
                 for (int j = 0; j < a.GetLength(1); j++)
-                    if (max <= a[i, j])
+                {
+                    bool left = j != 0 && a[i, j] > a[i, j - 1] || j == 0;
+                    bool right = j != n1 - 1 && a[i, j] > a[i, j + 1] || j == n1-1;
+                    bool up = i != 0 && a[i, j] > a[i - 1, j] || i == 0;
+                    bool down = i != n - 1 && a[i, j] > a[i + 1, j] || i == n-1;
+                    if (left && right && up && down)
                     {
-                        max = a[i, j];
-                        indexI = i;
-                        indexJ = j;
+                        count++;
                     }
+                }
             }
-            Console.WriteLine();
-            Console.WriteLine("Наибольший элемент массива: " + max);
-            Console.WriteLine("Индекс наибольшего элемента массива: (" + indexI + ", " + indexJ + ")");
+
+            Console.WriteLine($"Количество элементов: {count}");
+        }
+
         }
     }
-}
+
 
 
 
